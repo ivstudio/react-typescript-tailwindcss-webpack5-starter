9a89401102795c6b7a0531d34a9b83edc4613444
Iggy Villamar <iggy@ivstudio.com>
2024-08-03 13:41:06 -0400

    install tailwind and add directives

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import { Config } from 'tailwindcss';
+import { PluginAPI } from 'tailwindcss/types/config';
+
+const config: Config = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		function ({ addUtilities }: PluginAPI) {
+			const newUtilities = {
+				'.bg-smooth': {
+					transition: 'background 250ms ease-in-out',
+				},
+			};
+
+			addUtilities(newUtilities);
+		},
+	],
+};
+
+export default config;
