8254db784f24fcd55705165eb09ac6f9281d5cf0
Iggy Villamar <iggy@ivstudio.com>
2024-08-03 14:41:23 -0400

    install prettier and plugins for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ...pluginReact.configs.flat.recommended,
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+        },
+    },
+    prettier,
+    {
+        plugins: {
+            prettier: pluginPrettier,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+        },
+    },
 ];
