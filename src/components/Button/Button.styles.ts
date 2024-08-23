60901f2d5cac804c392bd05212dec7a0288903f6
ivstudio <ivstudio@users.noreply.github.com>
2024-08-23 03:13:21 +0000

    deploy: 898878fe6576c5c1c78d9488f440bb88a3f08029

--- src/components/Button/Button.styles.ts
+++
@@ -1,8 +0,0 @@
-const base = 'font-medium rounded-full px-5 py-2.5 text-center bg-smooth';
-
-const className = {
-    primary: `${base} text-white bg-blue-500 hover:bg-blue-600`,
-    secondary: `${base} text-gray-900 bg-white hover:bg-gray-100`,
-};
-
-export default className;
